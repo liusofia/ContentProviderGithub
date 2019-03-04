b23e4ed948b4d0215f752830d0d350c6740502a0
liuyixi <liuyixi@xiaomi.com>
2019-03-04 17:06:00 +0800

    sofia:add read Contact feature.

---
+++ ContentProviderContact/Contact/app/src/main/java/demo/contact/sofia/com/contact/MainActivity.java
@@ -0,0 +1,82 @@
+package demo.contact.sofia.com.contact;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * READ_CONTACTS属于危险权限
+     * 1：用户授权之后调用readContacts()方法来读取系统联系人信息
+     * 2：在readContacts中读取系统联系人信息
+     * 3：记得在Manifest中添加权限代码
+     * */
+    ArrayAdapter<String> adapter;
+
+    List<String> contactsList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView contactsView = (ListView) findViewById(R.id.contacts_view);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2,contactsList);
+        contactsView.setAdapter(adapter);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},1);
+        }else{
+            readContacts();
+        }
+    }
+
+    private void readContacts(){
+        Cursor cursor = null;
+        try{
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+            if(cursor != null){
+                while(cursor.moveToNext()){
+                    //获取联系人姓名
+                    String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    //获取联系人手机号
+                    String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contactsList.add(displayName + "\n" + number);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(cursor != null){
+                cursor.close();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    readContacts();
+                }else{
+                    Toast.makeText(this,"权限不允许",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+}
